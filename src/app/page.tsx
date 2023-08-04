f9e0a207836966cab017d6931b284f4a860121dd
Felipe Alcantara <felipemocruha@gmail.com>
2023-08-04 18:24:47 -0300

    chore: improve number generation algo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,31 @@ import { useState } from "react";
 import Image from 'next/image'
 import styles from './page.module.css'
 
+function getRandom(min, max) {
+    let range = max - min;
+    let bits_needed = Math.ceil(Math.log2(range + 1));
+    let bytes_needed = Math.ceil(bits_needed / 8);
+    let max_number = Math.pow(2, bits_needed);
+
+    let array = new Uint8Array(bytes_needed);
+
+    do {
+        window.crypto.getRandomValues(array);
+        var result = 0;
+        for (let i = 0; i < bytes_needed; i++) {
+            result = (result << 8) + array[i];
+        }
+    } while (result > max_number - max_number % (range + 1));
+
+    return result % (range + 1) + min;
+}
 
 function generate() {
   let numbers = [];
   let len = 0;
 
   for (let i = 0; i < 6; i++) {
-	numbers.push(Math.floor(Math.random() * 60) + 1);
+	numbers.push(getRandom(1, 60));
   }
 
   return numbers.sort((a,b) => a - b).join(" ");
